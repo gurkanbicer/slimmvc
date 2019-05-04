3c1ce6a3f549cdc5290aa8967a515f42edc544e9
Gürkan Biçer <gurkan@grkn.co>
2019-05-04 21:01:45 +0300

    Update index.php
    The description updated.

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 /**
  * SlimMVC
  * It provides an MVC structure for SlimPHP (Slim Framework).
-
+ *
  * Basically, all of the frameworks are provides a fast development environment. But, SlimPHP (Slim Framework) is a
  * micro framework and it becomes with minimal features like the routing, the request, and the response interfaces.
  * It hasn't an MVC structure. When you want to use SlimPHP and MVC structure, you should add all of the dependencies
  * as manually. It's tiring and it doesn't provide entirety.
  *
- * We got inspired by Laravel and CodeIgniter, and we prepared this environment for who likes the SlimPHP and working at the MVC structure.
+ * We got inspired by Laravel and CodeIgniter, and we prepared this environment for who likes the SlimPHP and working 
+ * at the MVC structure.
  *
  * @link https://www.slimmvc.com
  * @version v0.1
